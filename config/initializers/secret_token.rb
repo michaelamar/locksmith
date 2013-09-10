95ee20e3f705350dedd23213f2ff68026df2cd20
Michael Amar <Mordachiamar@gmail.com>
2013-09-10 13:12:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locksmith::Application.config.secret_token = 'cc4d1baeb5116570009a5b16d5bf04c77838361a059e1bfa47dbd8af3024fd6e882a8c4be3443b265e0299ebfb60a954e56dbb28df1d1e7bb6b425a2530a8277'
